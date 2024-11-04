86db8a4aa8a11a02319d302bcb00f1dfcfb64f4f
rafaeelricco <rafaelricco10@gmail.com>
2024-11-04 15:37:41 -0300

    feat: created initial checkbox component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,42 @@ import {
    AccordionTrigger
 } from '@/components/ui/accordion'
 import { Button } from '@/components/ui/button'
+import { Checkbox } from '@/components/ui/checkbox'
 import { Input } from '@/components/ui/input'
+import {
+   Select,
+   SelectContent,
+   SelectItem,
+   SelectTrigger
+} from '@/components/ui/select'
 
 import React from 'react'
 
 export default function Home() {
    return (
       <React.Fragment>
+         <div className="m-2 rounded-md p-2 border space-y-4 border-primary-default border-dashed">
+            <Checkbox label="Sem Termo de Apreensão" />
+         </div>
+         <div className="m-2 rounded-md p-2 border space-y-4 border-primary-default border-dashed">
+            <Select>
+               <SelectTrigger hideIcon className="w-fit border-none p-0">
+                  <Button asChild variant="ghost">
+                     <div className="flex !flex-row items-center gap-2">
+                        <span>Filtrar</span>
+                        <span className="material-symbols-outlined text-[1.5rem]">
+                           filter_alt
+                        </span>
+                     </div>
+                  </Button>
+               </SelectTrigger>
+               <SelectContent>
+                  <SelectItem value="light">Light</SelectItem>
+                  <SelectItem value="dark">Dark</SelectItem>
+                  <SelectItem value="system">System</SelectItem>
+               </SelectContent>
+            </Select>
+         </div>
          <div className="m-2 rounded-md p-2 border border-primary-default border-dashed">
             <Button variant="destructive">Click me</Button>
             <Button variant="default">Click me</Button>
