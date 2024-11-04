3c45de359cd766d3e1684f453e4961224a9f5b58
rafaeelricco <rafaelricco10@gmail.com>
2024-11-04 14:42:32 -0300

    feat: created intial custom accordion component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import {
+   Accordion,
+   AccordionContent,
+   AccordionItem,
+   AccordionTrigger
+} from '@/components/ui/accordion'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 
@@ -20,6 +26,14 @@ export default function Home() {
             <Input label="Email" type="email" />
             <Input label="Email" type="email" error="Error" />
             <Input label="Email" type="email" disabled />
+         </div>
+         <div className="m-2 rounded-md p-2 border space-y-4 border-primary-default border-dashed">
+            <Accordion type="single" collapsible>
+               <AccordionItem value="item-1">
+                  <AccordionTrigger>Pré-leilão</AccordionTrigger>
+                  <AccordionContent>Opções de pré-leilão</AccordionContent>
+               </AccordionItem>
+            </Accordion>
          </div>
       </React.Fragment>
    )
