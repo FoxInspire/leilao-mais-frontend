78ce3d14546be870eef31c25256c06a172f7c92c
rafaeelricco <rafaelricco10@gmail.com>
2024-11-07 16:07:08 -0300

    feat: implemented dark and light mode to login page

--- src/features/auth/login-form.tsx
+++ src/features/auth/login-form.tsx
@@ -1,23 +1,32 @@
 'use client'
 
-import { LoginIllustration } from '@/src/components/svgs/login'
+import {
+   LoginIllustrationDark,
+   LoginIllustrationLight
+} from '@/src/components/svgs/login'
 import { LogoComplete } from '@/src/components/svgs/logo'
 import { Button } from '@/src/components/ui/button'
 import { Input } from '@/src/components/ui/input'
 import { ThemeSwitch } from '@/src/components/ui/theme-switcher'
+import { useTheme } from 'next-themes'
 
 import React from 'react'
 
 const LoginForm: React.FC = () => {
+   const { theme } = useTheme()
    return (
       <React.Fragment>
-         <div className="grid lg:grid-cols-2 grid-cols-1 lg:gap-20 gap-10 min-h-screen p-4 lg:p-10">
-            <div className="bg-primary-default/70 hidden lg:flex flex-col justify-center items-center rounded-3xl">
-               <LoginIllustration className="max-w-full lg:max-w-xl" />
+         <div className="grid lg:grid-cols-2 grid-cols-1 gap-8 min-h-screen p-4 lg:p-10">
+            <div className="bg-primary-default/70 dark:bg-dark-primary-default/70 hidden lg:flex flex-col justify-center items-center rounded-3xl">
+               {theme === 'light' ? (
+                  <LoginIllustrationLight className="max-w-full lg:max-w-[500px]" />
+               ) : (
+                  <LoginIllustrationDark className="max-w-full lg:max-w-[500px]" />
+               )}
             </div>
-            <div className="flex gap-6 flex-col justify-center lg:max-w-2xl w-full mx-auto relative px-4">
+            <div className="flex gap-6 flex-col justify-center w-full mx-auto relative p-6">
                <LogoComplete className="w-56 lg:w-64 mx-auto my-0" />
-               <div className="flex gap-4 flex-col">
+               <div className="flex gap-6 flex-col">
                   <div className="space-y-1">
                      <h3 className="text-xl lg:text-2xl font-semibold font-montserrat">
                         Login
@@ -26,17 +35,19 @@ const LoginForm: React.FC = () => {
                         Insira suas credenciais para acessar a plataforma.
                      </p>
                   </div>
-                  <Input
-                     label="E-mail"
-                     type="email"
-                     placeholder="Digite seu e-mail"
-                  />
-                  <Input
-                     label="Senha"
-                     type="password"
-                     placeholder="Digite sua senha"
-                  />
-                  <span className="text-base font-normal text-primary-default hover:underline cursor-pointer text-end">
+                  <div className="space-y-4">
+                     <Input
+                        label="E-mail"
+                        type="email"
+                        placeholder="Digite seu e-mail"
+                     />
+                     <Input
+                        label="Senha"
+                        type="password"
+                        placeholder="Digite sua senha"
+                     />
+                  </div>
+                  <span className="text-base font-normal text-primary-default hover:underline cursor-pointer text-end dark:text-dark-primary-default">
                      Esqueceu sua senha?
                   </span>
                   <Button>Entrar</Button>
