e7a80a2aef036be57985d9452a4b149cdce0fc16
rafaeelricco <rafaelricco10@gmail.com>
2024-11-27 15:45:59 -0300

    feat: add seed script to generate initial auctions data
    - Added a seed script (`seed.ts`) to generate initial data for auctions.
    - The script uses back-end types to ensure consistency with the server.
    - Updated the `package.json` to include the seed script in the `dev` command.
    - Updated the `tsconfig.json` to include the `@/seeds/*` import path.
    - Added a new checkbox column to the auction lots table for row selection.
    
    Closes #123

--- src/features/pre-auction/auction-lots/components/columns.tsx
+++ src/features/pre-auction/auction-lots/components/columns.tsx
@@ -8,10 +8,37 @@ import { AuctionEntity } from '../types/auction.entity.ts'
 import { DataTableColumnHeader } from './data-table-column-header'
 import { DataTableRowActions } from './data-table-row-actions'
 
+import { Checkbox } from '@/src/components/ui/checkbox.jsx'
 import Link from 'next/link'
 import React from 'react'
 
 export const columns: ColumnDef<AuctionEntity>[] = [
+   {
+      id: 'select',
+      header: ({ table }) => (
+         <Checkbox
+            checked={
+               table.getIsAllPageRowsSelected() ||
+               (table.getIsSomePageRowsSelected() && 'indeterminate')
+            }
+            onCheckedChange={(value) =>
+               table.toggleAllPageRowsSelected(!!value)
+            }
+            aria-label="Select all"
+            className="translate-y-[2px]"
+         />
+      ),
+      cell: ({ row }) => (
+         <Checkbox
+            checked={row.getIsSelected()}
+            onCheckedChange={(value) => row.toggleSelected(!!value)}
+            aria-label="Select row"
+            className="translate-y-[2px]"
+         />
+      ),
+      enableSorting: false,
+      enableHiding: false
+   },
    {
       accessorKey: 'auctionDate',
       header: ({ column }) => (
