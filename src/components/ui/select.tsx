de170872f128d868fa47259f5c5df55d2986d99e
rafaeelricco <rafaelricco10@gmail.com>
2024-11-29 16:05:32 -0300

    refactor: enhance SelectTrigger styling in DataTablePagination
    - Updated SelectTrigger component's className to improve styling consistency, adding a dark mode border for better visibility.
    - Adjusted the SelectTrigger usage in DataTablePagination to reflect the new styling changes, ensuring a cohesive user experience across components.

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -46,7 +46,7 @@ const SelectTrigger = React.forwardRef<
    <SelectPrimitive.Trigger
       ref={ref}
       className={cn(
-         'flex h-10 w-full items-center justify-between rounded-md border border-neutral-200 bg-white px-3 py-2 text-sm ring-offset-white placeholder:text-neutral-500 focus:outline-none focus:ring-2 focus:ring-neutral-950 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 dark:border-neutral-800 dark:bg-neutral-950 dark:ring-offset-neutral-950 dark:placeholder:text-neutral-400 dark:focus:ring-neutral-300 dark:bg-transparent',
+         'flex h-10 w-full items-center justify-between rounded-md border border-neutral-200 bg-white px-3 py-2 text-sm ring-offset-white placeholder:text-neutral-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 dark:border-neutral-800 dark:bg-neutral-950 dark:ring-offset-neutral-950 dark:placeholder:text-neutral-400 dark:focus-visible:ring-neutral-300 dark:bg-transparent',
          className
       )}
       {...props}
